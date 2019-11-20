180ab006f08383620ac443a9ba2b074485ec0669
weizhetao <38481676+weizt@users.noreply.github.com>
2019-11-20 16:53:59 +0800

    Add files via upload

---
+++ blogproject/blog/views.py
@@ -0,0 +1,56 @@
+'''
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
+'''
+import markdown
+from django.shortcuts import render, get_object_or_404
+from .models import Post,Category,Tag
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+import re
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+    return render(request, 'blog/detail.html', context={'post': post})
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def tag(request, pk):
+    # 记得在开始部分导入 Tag 类
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
